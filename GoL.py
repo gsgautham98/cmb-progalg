bd5dce6e473ec46a5382d8bded5a7783c4e75c22
Gautham Ganesh <68129413+gsgautham98@users.noreply.github.com>
2021-09-28 15:51:09 +0200

    Add files via upload

--- GoL.py
+++ GoL.py
@@ -3,14 +3,15 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 def random_grid(cells_x, cells_y):
-    initial_space = np.array()
-    for i in range(cells_x):
-        np.append(np.random.randint(0, 2, cells_y))
-    return initial_space
+    initialise_space = []
+    for _ in range(cells_x):
+        initialise_space.append(np.random.randint(0, 2, cells_y))
+    return initialise_space
 
 def update_anime_func(frame_number, img, gsp):
-    for i in range(32):
-        for j in range(32):
+    global rows, columns
+    for i in range(rows):
+        for j in range(columns):
             try:
                 neighbours = gsp[i][j+1] + gsp[i][j-1] + gsp[i-1][j] + gsp[i-1][j-1] + gsp[i-1][j+1] + gsp[i+1][j+1] + gsp[i+1][j] + gsp[i+1][j-1]
             except IndexError:
@@ -24,13 +25,29 @@ def update_anime_func(frame_number, img, gsp):
     img.set_data(gsp)
     return img
 
-with open("gamespace", "r") as gsp_file:
-    initialise_space = []
-    for x in [line.strip().split() for line in gsp_file.read().strip().split("\n")]:
-        initialise_space.append([int(i) for i in list(x[0])])
+def read_from_file():
+    with open("gamespace", "r") as gsp_file:
+        initialise_space = []
+        for x in [line.strip().split() for line in gsp_file.read().strip().split("\n")]:
+            initialise_space.append([int(i) for i in list(x[0])])
+        return initialise_space
 
-gsp = np.array(initialise_space)
+if __name__=="__main__":
+    question = input("Read from file (f) or create random space (r) ? ")
+    if question in "fF":
+        gsp = np.array(read_from_file())
+        rows = len(gsp)
+        columns = len(gsp[0])
+    elif question in "rR":
+        rows = int(input("Enter rows "))
+        columns = int(input("Enter columns "))
+        gsp = np.array(random_grid(rows, columns))
+    else:
+        print("Invalid input")
+        exit()
+        
 figure, ax = plt.subplots()
 img = ax.imshow(gsp, cmap="gray")
-anime = FuncAnimation(figure, update_anime_func, fargs=(img, gsp,), frames=500, repeat=False, interval=200)
+anime = FuncAnimation(figure, update_anime_func, fargs=(img, gsp,), frames=600, repeat=False, interval=100)
+plt.title("The Game of Life")
 plt.show()
